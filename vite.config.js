3b8fb9f74bef1c5307c90a7dfaf6fb29440be5bc
Gustavo Corpus <guscorpus40@gmail.com>
2025-03-31 17:23:32 -0600

    Prueba de errores

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/repaso/' // Cambia esto para que coincida con el nombre de tu repositorio
+  base: '/repaso/',  // Esto coincide con tu URL de GitHub Pages
 })
